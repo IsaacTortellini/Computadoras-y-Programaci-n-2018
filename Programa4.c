fc8433158698d8503b6e96bc29f034f7429cee37
Daniel Isaac <31526653+IsaacTortellini@users.noreply.github.com>
2017-09-26 11:39:44 -0500

    Tarea 8

---
+++ Programa4.c
@@ -0,0 +1,20 @@
+//PROGRAMA CUADRO DE SIMBOLOS POR DANIEL ISAAC REYES SANCHEZ
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    int j=0;
+    int k=0;
+    for(j=1;j<6;j=j+1){
+                       for(k=1;k<=6;k=k+1){
+                                           printf("+");
+                                               
+                                           }
+                                           printf("\n");
+                                           }
+  
+  system("PAUSE");	
+  return 0;
+}
+
