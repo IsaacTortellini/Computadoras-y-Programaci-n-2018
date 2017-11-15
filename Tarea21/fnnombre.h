f79c5c6a6ce415bdbfb8b045de76aa6f0a8b899c
Daniel Isaac <31526653+IsaacTortellini@users.noreply.github.com>
2017-11-15 13:27:08 -0600

    Tarea 21

---
+++ Tarea21/fnnombre.h
@@ -0,0 +1,9 @@
+ void fnnombre(){
+  
+          printf("iiii  ssss  aaaa  aaaa  cccc\n");
+          printf(" ii   ss    a  a  a  a  cc  \n");
+          printf(" ii   ssss  aaaa  aaaa  cc  \n");
+          printf(" ii     ss  a  a  a  a  cc  \n");
+          printf("iiii  ssss  a  a  a  a  cccc\n");
+
+  }
---
+++ Tarea21/nombreconfuncion.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "fnnombre.h"
+int main(int argc, char *argv[])
+{ 
+    fnnombre();
+    
+  system("PAUSE");	
+  return 0;
+}
